71cae147de9ec75af204f9aeae7fef695303bca9
zerocho <zerohch0@gmail.com>
2023-10-28 03:07:06 +0900

    3-2

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -6,6 +6,7 @@ import 'dayjs/locale/ko';
 import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
 import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
 import {faker} from '@faker-js/faker';
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime)
@@ -13,7 +14,7 @@ dayjs.extend(relativeTime)
 type Props = {
   noImage?: boolean
 }
-export default function Post({ noImage}: Props) {
+export default function Post({ noImage }: Props) {
   const target = {
     postId: 1,
     User: {
@@ -27,7 +28,10 @@ export default function Post({ noImage}: Props) {
   }
   if (Math.random() > 0.5 && !noImage) {
     target.Images.push(
-      {imageId: 1, link: faker.image.urlLoremFlickr()}
+      {imageId: 1, link: faker.image.urlLoremFlickr()},
+      {imageId: 2, link: faker.image.urlLoremFlickr()},
+      {imageId: 3, link: faker.image.urlLoremFlickr()},
+      {imageId: 4, link: faker.image.urlLoremFlickr()},
     )
   }
 
@@ -53,15 +57,8 @@ export default function Post({ noImage}: Props) {
             <span className={style.postDate}>{dayjs(target.createdAt).fromNow(true)}</span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}>
-            {target.Images && target.Images.length > 0 && (
-              <Link
-                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                className={style.postImageSection}
-              >
-                <img src={target.Images[0]?.link} alt=""/>
-              </Link>
-            )}
+          <div>
+            <PostImages post={target} />
           </div>
           <ActionButtons/>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import {MSWComponent} from "@/app/_component/MSWComponent";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <MSWComponent />
         {children}
       </body>
     </html>
