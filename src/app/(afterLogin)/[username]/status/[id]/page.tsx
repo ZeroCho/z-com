edb3541b284d8997fa029605e48353d9e49b6ff4
zerocho <zerohch0@gmail.com>
2023-11-19 19:58:52 +0900

    Update: openGraph

--- src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -20,17 +20,19 @@ export async function generateMetadata({params}: Props) {
     openGraph: {
       title: `Z에서 ${user.nickname} 님 : ${post.content}`,
       description: post.content,
-      images: post.Images?.map((v) => ({
-        url: `https://z.nodebird.com${v.link}`,
-        width: 600,
-        height: 400,
-      })) || [
-        {
-          url: `https://z.nodebird.com${user.image}`,
-          width: 400,
+      images: post.Images?.length > 0
+        ? post.Images?.map((v) => ({
+          url: `https://z.nodebird.com${v.link}`,
+          width: 600,
           height: 400,
-        },
-      ],
+        }))
+        : [
+          {
+            url: `https://z.nodebird.com${user.image}`,
+            width: 400,
+            height: 400,
+          },
+        ],
     }
   }
 }
