7bda7ae84f277b738ab7b21892dff791bc53a233
zerocho <zerohch0@gmail.com>
2023-12-04 01:21:49 +0900

    Fix: enter 시 메시지 전송되게(빈 칸은 안 되게)

--- src/app/(afterLogin)/messages/[room]/_component/MessageForm.tsx
+++ src/app/(afterLogin)/messages/[room]/_component/MessageForm.tsx
@@ -2,7 +2,7 @@
 
 import style from './messageForm.module.css';
 import TextareaAutosize from "react-textarea-autosize";
-import {ChangeEventHandler, FormEventHandler, useEffect, useState} from "react";
+import {ChangeEventHandler, KeyboardEventHandler, useState} from "react";
 import useSocket from "@/app/(afterLogin)/messages/[room]/_lib/useSocket";
 import {useSession} from "next-auth/react";
 import {InfiniteData, useQueryClient} from "@tanstack/react-query";
@@ -21,8 +21,7 @@ export default function MessageForm({ id }: Props) {
   const onChangeContent: ChangeEventHandler<HTMLTextAreaElement> = (e) => {
     setContent(e.target.value)
   }
-  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
+  const onSubmit = () => {
     if (!session?.user?.email) {
       return;
     }
@@ -61,10 +60,28 @@ export default function MessageForm({ id }: Props) {
     setContent('');
   }
 
+  const onEnter: KeyboardEventHandler<HTMLTextAreaElement> = (e) => {
+    console.log(e.key === 'Enter', e);
+    if (e.key === 'Enter') {
+      if (e.shiftKey) {
+        return;
+      }
+      e.preventDefault();
+      if (!content?.trim()) {
+        return;
+      }
+      onSubmit();
+      setContent('');
+    }
+  }
+
   return (
     <div className={style.formZone}>
-      <form className={style.form} onSubmit={onSubmit}>
-        <TextareaAutosize value={content} onChange={onChangeContent} />
+      <form className={style.form} onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit()
+      }}>
+        <TextareaAutosize value={content} onChange={onChangeContent} onKeyDown={onEnter} />
         <button className={style.submitButton} type="submit" disabled={!content}>
           <svg viewBox="0 0 24 24" width={18} aria-hidden="true" className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-z80fyv r-19wmn03"><g><path d="M2.504 21.866l.526-2.108C3.04 19.719 4 15.823 4 12s-.96-7.719-.97-7.757l-.527-2.109L22.236 12 2.504 21.866zM5.981 13c-.072 1.962-.34 3.833-.583 5.183L17.764 12 5.398 5.818c.242 1.349.51 3.221.583 5.183H10v2H5.981z"></path></g></svg>
         </button>
